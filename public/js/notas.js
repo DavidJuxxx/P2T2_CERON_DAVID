1f928c02b61cd0db4b68b1dc894ae75a6fcc726a
DavidJuxxx <cerondavid802@gmail.com>
2025-12-16 22:02:16 -0500

    Actualizando el index con nuev

--- public/js/notas.js
+++ public/js/notas.js
@@ -1,11 +1,8 @@
 let notas = [8, 4, 10, 6, 3, 9, 5, 7, 2];
 
-let aprobados = 0;
-let supletorio = 0;
-let reprobados = 0;
-let suma = 0;
-let promedio = 0;
-let estado = "";
+let txt_nota = document.getElementById("txt_nota");
+let btn_agregar = document.getElementById("btn_agregar");
+let btn_limpiar = document.getElementById("btn_limpiar");
 
 let txt_aprobados = document.getElementById("aprobados");
 let txt_supletorio = document.getElementById("supletorio");
@@ -13,30 +10,51 @@ let txt_reprobados = document.getElementById("reprobados");
 let txt_promedio = document.getElementById("promedio");
 let txt_estado = document.getElementById("estado");
 
-notas.forEach(function (nota) {
+btn_agregar.addEventListener("click", function () {
+    let nota = Number(txt_nota.value);
 
-    suma = suma + nota;
-
-    if (nota >= 7 && nota <= 10) {
-        aprobados = aprobados + 1;
-    } else if (nota >= 5 && nota <= 6) {
-        supletorio = supletorio + 1;
-    } else if (nota >= 0 && nota <= 4) {
-        reprobados = reprobados + 1;
-    }
-
+    notas.push(nota);
+    calcular();
+    txt_nota.value = "";
 });
 
-promedio = suma / notas.length;
+btn_limpiar.addEventListener("click", function () {
+    notas = [];
+    txt_nota.value = "";
 
-if (promedio >= 7) {
-    estado = "Aprobado";
-} else {
-    estado = "En riesgo";
+    txt_aprobados.value = "";
+    txt_supletorio.value = "";
+    txt_reprobados.value = "";
+    txt_promedio.value = "";
+    txt_estado.value = "";
+});
+
+function calcular() {
+    let aprobados = 0;
+    let supletorio = 0;
+    let reprobados = 0;
+    let suma = 0;
+
+    notas.forEach(function (nota) {
+        suma += nota;
+
+        if (nota >= 7) aprobados++;
+        else if (nota >= 5) supletorio++;
+        else reprobados++;
+    });
+
+    let promedio = suma / notas.length;
+    let estado = "";
+
+    if (promedio >= 7) {
+        estado = "Aprobado";
+    } else {
+        estado = "En riesgo";
+    }
+
+    txt_aprobados.value = aprobados;
+    txt_supletorio.value = supletorio;
+    txt_reprobados.value = reprobados;
+    txt_promedio.value = promedio;
+    txt_estado.value = estado;
 }
-
-txt_aprobados.value = aprobados;
-txt_supletorio.value = supletorio;
-txt_reprobados.value = reprobados;
-txt_promedio.value = promedio;
-txt_estado.value = estado;
